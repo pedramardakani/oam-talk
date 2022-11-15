f75af4eea7405a45f6dff5d7ce67c3eb3c9f5459
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2022-11-15 11:22:35 +0330

    Add licensing, upload raw txt file

--- script-gnuastro.bash
+++ script-gnuastro.bash
@@ -1,3 +1,3 @@
 asttable catalog-raw.fits -cID,SPECZ,10,11,12,'arith $10 $12 -' --sort=SPECZ \
 | asttable  --range=6,3:inf --txtf64format fixed \
-| grep -ve' -\?99.0*0 ' > gnuastro-reddest-with-z.txt
+| grep -ve' -99.0*0 ' -ve ' 99.0*0 ' > gnuastro-reddest-with-z.txt
